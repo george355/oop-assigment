75fe8ae92b9acd4f3d7234d4e5d0ffadf43b9b9c
george355 <ciobanugeorge030801@gmail.com>
2022-04-06 20:06:37 +0100

    Add files via upload

--- control.java
+++ control.java
@@ -1,46 +1,24 @@
 package assigment;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class control {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		new file_selector();
+		file_selector test = new file_selector();
+		test.file_selector1();
 		
-	        // list that holds strings of a file
-	        List<String> listOfStrings
-	            = new ArrayList<String>();
-	       
-	        // load data from file
-	        BufferedReader bf = new BufferedReader(
-	            new FileReader("text.txt"));
-	       
-	        // read entire line as string
-	        String line = bf.readLine();
-	       
-	        // checking for end of file
-	        while (line != null) {
-	            listOfStrings.add(line);
-	            line = bf.readLine();
-	        }
-	       
-	        // closing bufferreader object
-	        bf.close();
-	       
-	        // storing the data in arraylist to array
-	        String[] array
-	            = listOfStrings.toArray(new String[0]);
-	       
-	        // printing each line of file
-	        // which is stored in array
-	        for (String str : array) {
-	            System.out.println(str);
-	        }
+
+		
+
+		
+		
+		
+		
+		
+		
+	      
 }
 }
 

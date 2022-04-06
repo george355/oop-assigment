46677defa0fdf6acbcdf741ac81ee1cd6b67ab67
george355 <ciobanugeorge030801@gmail.com>
2022-04-06 20:06:52 +0100

    Add files via upload

--- file_selector.java
+++ file_selector.java
@@ -1,15 +1,14 @@
 package assigment;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.lang.module.ModuleDescriptor.Exports;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -26,47 +25,50 @@ public class file_selector extends JFrame implements ActionListener{
 	Scanner sc = null;
 	String one;
 	String two;
-	
-	public String search;
-	
+	String search;
+		
+		JPanel panel;
+		JFrame GUI;
 		JButton button1;
 		JButton button2;
 		JTextField textfield1;
 		JButton button3;
 
-	file_selector (){
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout( new FlowLayout());
+	public void file_selector1(){
+		GUI=new JFrame("My Search Engine");
+		panel = new JPanel(null);
+		panel.setLayout( new FlowLayout());
+		panel.setBackground(Color.CYAN);
+		GUI.add(panel);
 		
 		button1= new JButton("select first file");
 		button1.addActionListener(this);
-		
 		button2= new JButton("select second file");
 		button2.addActionListener(this);
-		
 		textfield1 = new JTextField();
 		textfield1.setText("Please enter you word");
-		
 		button3= new JButton("Search");
 		button3.addActionListener(this);
-		
-		this.add(button1);
-		this.add(button2);
-		this.add(textfield1);
-		this.add(button3);
-		this.pack();
-		this.setVisible(true);
+
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(textfield1);
+		panel.add(button3);
+
+
+		GUI.setVisible(true);
+		GUI.setSize(400,400);
+		GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GUI.setLocationRelativeTo(null);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		
 		if(e.getSource()==button1) {
 			JFileChooser fileChooser = new JFileChooser();
 			response1 = fileChooser.showOpenDialog(null);
 			
 			if(response1 == JFileChooser.APPROVE_OPTION) {
 				File file1 =new File(fileChooser.getSelectedFile().getAbsolutePath());
-				System.out.println(file1);
 				// arraylist to store strings
 		        List<String> listOfStrings = new ArrayList<String>();
 		       
@@ -90,18 +92,23 @@ public class file_selector extends JFrame implements ActionListener{
 		       
 		        // print each string in array
 		        for (String eachString : array) {
-		           System.out.println(eachString);
-		           one=eachString;
-		        }}}
+		           one=one+eachString;
+		        }
+		        if (one!=null)
+		        	System.out.println("A file which is not empty has been selected");
+		        else
+		        	System.out.println("An empty file has been selected");}
+			}
+		
 		
 		
 	if(e.getSource()==button2) {
+			two=null;
 			JFileChooser fileChooser = new JFileChooser();
 			response2 = fileChooser.showOpenDialog(null);
 			
 			if(response2 == JFileChooser.APPROVE_OPTION) {
 				File file2 =new File(fileChooser.getSelectedFile().getAbsolutePath());
-				System.out.println(file2);
 				
 		        // arraylist to store strings
 		        List<String> listOfStrings= new ArrayList<String>();
@@ -125,27 +132,30 @@ public class file_selector extends JFrame implements ActionListener{
 		       
 		        // print each string in array
 		        for (String eachString : array) {
-		            System.out.println(eachString);
 		            two=two+eachString;
 		        }
-		        System.out.println(two);
+		        if (two!=null)
+		        	System.out.println("A file which is not empty has been selected");
+		        else
+		        	System.out.println("An empty file has been selected");
+		        
 		        }}
-	
+try {
 		if(e.getSource()==button3) {
-			//System.out.println(one);
-			//System.out.println(two);
-			
+			int count1=0;
+			int count2=0;
 			search=textfield1.getText();
-			System.out.println(search);
-
+			
+			try {
 			if (one.contains(search)) {
+				System.out.println("\n\nMatches from the first file");
+				
 				 String[] sp = one.split(" +"); // "+" for multiple spaces
 
 				    String[] spMulti = search.split(" +"); // "+" for multiple spaces
 				    for (int i = 0; i < sp.length; i++) {
 				        int j = 0;
-				        while (j < spMulti.length && i+j < sp.length 
-				                                  && sp[i+j].equals(spMulti[j])) {
+				        while (j < spMulti.length && i+j < sp.length && sp[i+j].equals(spMulti[j])) {
 				            j++;
 				        }           
 				        if (j == spMulti.length) { // found spMulti entirely
@@ -165,20 +175,26 @@ public class file_selector extends JFrame implements ActionListener{
 				                surr.append(sp[i+spMulti.length+1]);
 				            }
 				            System.out.println(surr.toString());
+				            count1=count1 +1;
 				        }
 				    }
 			}
-			
+			else 
+				System.out.println("\nThe word you have enter is not found in the first file");
+			}
+			catch (Exception f2) {
+				System.out.println("First file not selected");
+			}
+			try {
 			if (two.contains(search)) {
-				 System.out.println("fuck this");
+					System.out.println("\n\nMatches from the second file");
 
 				    String[] sp = two.split(" +"); // "+" for multiple spaces
 
 				    String[] spMulti = search.split(" +"); // "+" for multiple spaces
 				    for (int i = 0; i < sp.length; i++) {
 				        int j = 0;
-				        while (j < spMulti.length && i+j < sp.length 
-				                                  && sp[i+j].equals(spMulti[j])) {
+				        while (j < spMulti.length && i+j < sp.length && sp[i+j].equals(spMulti[j])) {
 				            j++;
 				        }           
 				        if (j == spMulti.length) { // found spMulti entirely
@@ -195,14 +211,39 @@ public class file_selector extends JFrame implements ActionListener{
 				            }
 				            if (i+spMulti.length+1 < sp.length) {
 				                surr.append(" ");
-				                surr.append(sp[i+spMulti.length+1]);
+				                surr.append(sp[i+spMulti.length+1]);		                
 				            }
 				            System.out.println(surr.toString());
+				            count2=count2 +1;
 				        }
 				    }
 				}
-		}}}
+			else 
+				System.out.println("\nThe word you have enter is not found in the second file");
+			}
+			catch (Exception f2) {
+				System.out.println("Second file not selected");
+			}
+			
+			if ( textfield1.getText().isBlank() || textfield1.getText().equals("Please enter you word")) {
+				System.out.println("You havent enter a search word");}
+			
+			else	
+				if (count1>count2 && count1!=count2) {
+					System.out.println("\nFirst file has more matches than the second one");
+				}
+				else if (count2>count1 && count2!=count1) {
+					System.out.println("\nSecond file has more matches than the first one");
+				}
 				
+				else 
+					System.out.println("\nBoth files have the same amount of matches");
+			
+			
+		}
+}
+	
+	catch (Exception f) {
+		System.out.println("Files not selected");}
 
-
-		
+}}			
